46c909d35dcadf8de410b4d81b7cf37b77c06446
jaoviedo <jaoviedo@umail.ucsb.edu>
2021-05-24 14:35:06 -0700

    Tweaking date counts

--- project-code/date.h
+++ project-code/date.h
@@ -142,82 +142,38 @@ public:
             string date = inDate.substr(8, 2);
             if (year == "2015")
             {
-                if (counts_2015.count(month) > 0)
-                {
-                    counts_2015[month] = 1;
-                }
-                else
-                {
-                    counts_2015[month] = counts_2015[month]++;
-                }
+                counts_2015[month]++;
             }
             if (year == "2016")
             {
-                if (counts_2016.count(month) > 0)
-                {
-                    counts_2016[month] = 1;
-                }
-                else
-                {
-                    counts_2016[month] = counts_2016[month]++;
-                }
+                counts_2016[month]++;
+
             }
             if (year == "2017")
             {
-                if (counts_2017.count(month) > 0)
-                {
-                    counts_2017[month] = 1;
-                }
-                else
-                {
-                    counts_2017[month] = counts_2017[month]++;
-                }
+                counts_2017[month]++;
+
             }
             if (year == "2018")
             {
-                if (counts_2018.count(month) > 0)
-                {
-                    counts_2018[month] = 1;
-                }
-                else
-                {
-                    counts_2018[month] = counts_2018[month]++;
-                }
+                counts_2018[month]++;
+
+
             }
             if (year == "2019")
             {
-                if (counts_2019.count(month) > 0)
-                {
-                    counts_2019[month] = 1;
-                }
-                else
-                {
-                    counts_2019[month] = counts_2019[month]++;
-                }
+
+                counts_2019[month]++;
+
             }
             if (year == "2020")
             {
-                if (counts_2020.count(month) > 0)
-                {
-                    counts_2020[month] = 1;
-                }
-                else
-                {
-                    cout << "Before " << counts_2020[month] << endl;
-                    counts_2020[month] += counts_2020[month];
-                    cout << "After: " << counts_2020[month] << endl;
-                }
+
+                counts_2020[month]++;
             }
             if (year == "2021")
             {
-                if (counts_2021.count(month) > 0)
-                {
-                    counts_2021[month] = 1;
-                }
-                else
-                {
-                    counts_2021[month] = counts_2021[month]++;
-                }
+                counts_2021[month]++;
             }
         }
     }
